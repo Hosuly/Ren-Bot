ee6a6ae328666db0808f5c50b2c8c1f75f35a2e3
Hosuly <74451306+Hosuly@users.noreply.github.com>
2022-04-14 15:22:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.snipes = new Map();
 
 ['command', 'event', 'slash'].forEach( handler => require(`./handlers/${handler}`)(client) );
 
-client.on('messageDelete', function(message, channel){ //Sn
+client.on('messageDelete', function(message, channel){ //Snipe code
     client.snipes.set(message.channel.id, {
         content: message.content,
         author: message.author.id,
