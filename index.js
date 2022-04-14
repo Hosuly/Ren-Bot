14340a683188f31e8c56bb0ef62a28cf0d328c5c
Hosuly <74451306+Hosuly@users.noreply.github.com>
2022-04-14 15:11:17 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Client, Intents, Collection } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const config = require('./config.json');
+const fs = require('fs');
+client.commands = new Collection();
+client.aliases = new Collection();
+client.categories = new Collection();
+client.interactions = new Collection();
+client.snipes = new Map();
+
+['command', 'event', 'slash'].forEach( handler => require(`./handlers/${handler}`)(client) );
+
+client.on('messageDelete', function(message, channel){
+    client.snipes.set(message.channel.id, {
+        content: message.content,
+        author: message.author.id,
+        image: message.attachments.first() ? message.attachments.first().proxyURL : null
+    })
+})
+
+client.login(config.token)
