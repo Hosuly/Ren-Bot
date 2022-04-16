da6195ff6fe341da8a7db712c9a4d5827edf5595
Hosuly <74451306+Hosuly@users.noreply.github.com>
2022-04-16 17:59:19 +0700

    Delete tps.js

--- commands/2y2c/tps.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'tps',
-    category: '2y2c',
-    aliases: [],
-    execute(bot, message, args, username)  {
-        bot.chat(`/msg ${username} Current tps: ${bot.getTps()}`)
-        
-    }
-}
