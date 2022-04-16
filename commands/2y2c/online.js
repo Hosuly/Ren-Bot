086158f71bec7babfe3061c2952f15ac1e8ef8d1
Hosuly <74451306+Hosuly@users.noreply.github.com>
2022-04-16 17:59:07 +0700

    Delete online.js

--- commands/2y2c/online.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'online',
-    category: '2y2c',
-    aliases: [],
-    execute(bot, message, args, username)  {
-        bot.chat(`/msg ${username} Player online: ${Object.values(bot.players).map(name => name.username).length}`)       
-    }
-}
