0edd92863abc8487c86f1321ee9851a7adaa0ca2
Hosuly <74451306+Hosuly@users.noreply.github.com>
2022-04-24 04:29:24 +0700

    Delete events directory

--- events/messageCreate.js
+++
@@ -1,9 +0,0 @@
-module.exports = (client, message) => {
-    if (message.author.bot) return
-    const prefix = 'r:'
-    if (!message.content.startsWith(prefix)) return
-    const args = message.content.slice(prefix.length).trim().split(' ')
-    const ear = args.shift().toLowerCase()
-    const command = client.commands.get(ear) || client.commands.get(client.aliases.get(ear))
-    if (command) command.run(client, message, args)
-}
--- events/ready.js
+++
@@ -1,8 +0,0 @@
-module.exports = (client) => {
-    console.log('Ren is wake up!')
-
-    client.user.setActivity({
-        name: 'Learning with Hosuly',
-        type: 'STREAMING'
-    }
-)}
