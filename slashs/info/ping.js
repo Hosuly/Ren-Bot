210f72dda24604399c51c76985f7086559174044
Hosuly <74451306+Hosuly@users.noreply.github.com>
2022-04-24 04:29:45 +0700

    Delete slashs/info directory

--- slashs/info/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: 'Xem Ping Của Bot',
-    type: 'CHAT_INPUT',
-    run: async (client, interaction) => {
-        interaction.reply(`${client.ws.ping}ms`)
-    },
-}
